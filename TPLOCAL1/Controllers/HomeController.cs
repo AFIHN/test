b70d1c3e8716c500e918b74267d2e7b78845a98c
AFIHN <afialaire@hn-services.com>
2024-07-30 11:46:50 +0200

    first commit

---
+++ TPLOCAL1/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TPLOCAL1.Models;
+
+//Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
+//--------------------------------------------------------------------------------------
+//Careful, the MVC model is a so-called convention model instead of configuration,
+//The controller must imperatively be name with "Controller" at the end !!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //methode "naturally" call by router
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //retourn to the Index view (see routing in Program.cs)
+                return View();
+            else
+            {
+                //Call different pages, according to the id pass as parameter
+                switch (id)
+                {
+                    case "OpinionList":
+                        //TODO : code reading of the xml files provide
+                        OpinionList opinionList = new OpinionList();
+                        List<Opinion> model = opinionList.GetAvis("XlmFile/DataAvis.xml");
+                        return View(id,model);
+                    case "Form":
+                        //TODO : call the Form view with data model empty
+                        return View(id);
+                    default:
+                        //retourn to the Index view (see routing in Program.cs)
+                        return View();
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormModel model)
+        {
+            return View(model);
+        }
+
+        //methode to send datas from form to validation page
+        public ActionResult ValidationFormulaire(FormModel model)
+        {
+            //TODO : test if model's fields are set
+            //if not, display an error message and stay on the form page
+            //else, call ValidationForm with the datas set by the user
+            if (!ModelState.IsValid)
+            {
+
+                return View("Form", model);
+            }
+
+            return View(model);
+
+        }
+    }
+}
---
+++ TPLOCAL1/Models/FormModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class FormModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string? FirstName {  get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Gender { get; set; }
+
+        [Required]
+        public string? Address { get; set; }
+
+        [Required]
+        [RegularExpression("[0-9][0-9][0-9][0-9][0-9]",ErrorMessage = "Must be of format nnnnn")]
+        public string? Zipcode { get; set; }
+
+        [Required]
+        public string? Town { get; set; }
+
+        [Required]
+        [RegularExpression("^[\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$", ErrorMessage = "Please put an email")]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Starting Date")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Display(Name = "Type of Training")]
+        public string? Typeoftraining { get; set; }
+
+        [Display(Name = "Cobol Training")]
+        public string? CobolTraining { get; set; }
+
+        [Display(Name = "Purpose of Training")]
+        public string? TrainingPurpose { get; set; }
+
+    }
+}
